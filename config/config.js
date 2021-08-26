dbb00d273c29640f2ed2efc601658ff3b13915c8
Javier Toledo <kingsmantoledo@gmail.com>
2021-08-25 19:39:27 -0500

    Deploy a heroku

--- config/config.js
+++ config/config.js
@@ -6,20 +6,26 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    logging: false
   }
 };
