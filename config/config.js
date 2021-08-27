6e7b68d8c66e5684024186dba80d6c93ffc895ce
Javier Toledo <kingsmantoledo@gmail.com>
2021-08-27 00:02:11 -0500

    Corrección Código

--- config/config.js
+++ config/config.js
@@ -6,26 +6,20 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: { ssl: { rejectUnauthorized: false, ssl: true } }
+    dialect: "mysql"
   },
   test: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: { ssl: { rejectUnauthorized: false, ssl: true } }
+    dialect: "mysql"
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: { ssl: { rejectUnauthorized: false, ssl: true } }
+    dialect: "mysql"
   }
 };
